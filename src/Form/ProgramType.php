b01b17065002b5ee9faaa49633c14728113150dc
Aline Denizot <aline.denizot2@gmail.com>
2020-12-08 19:51:05 +0100

    quete 15 : entity type

--- src/Form/ProgramType.php
+++ src/Form/ProgramType.php
@@ -3,6 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Program;
+use App\Entity\Actor;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -18,6 +20,12 @@ class ProgramType extends AbstractType
             ->add('summary', TextType::class)
             ->add('poster', TextType::class)
             ->add('category', null, ['choice_label' => 'name'])
+            ->add('actors', EntityType::class, [
+                'class' => Actor::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,
+        ])
         ;
     }
 
